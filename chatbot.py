753908452b41f71a44432e2f83f750c8536810e9
Nehal Vaghasiya <nehal.vaghasiya@sttech.de>
2023-07-09 17:44:19 +0200

    refactor the code

--- chatbot.py
+++ chatbot.py
@@ -5,8 +5,14 @@ from utils import get_completion, get_questions
 from config import Parameters
 
 class InterviewBot:
+    """
+    Class representing the Interview chatbot.
+    """
+
     def __init__(self) -> None:
-        # Checking if session variables exist, if not initialize them
+        """
+        Initialize the InterviewBot and its session state.
+        """
         if 'questions' not in st.session_state:
             st.session_state['questions'] = []
 
@@ -16,67 +22,84 @@ class InterviewBot:
         if 'interview_step' not in st.session_state:
             st.session_state['interview_step'] = 0
 
+        self.session_state = st.session_state
+
     def conduct_interview(self) -> None:
-        # Get questions from the OpenAI API based on the job description
+        """
+        Conduct the interview by generating questions from the OpenAI API and Job description.
+        """
         text_questions = get_completion(Parameters.questions_prompt.format(job_description=Parameters.job_description))
+
         questions = get_questions(text_questions)
-        # Append the questions with unique UUIDs to the session variable
-        for question in questions:
-            st.session_state['questions'].append((question, str(uuid.uuid4())))
+
+        self.session_state['questions'] = [(question, self._generate_uuid()) for question in questions]
 
     def ask_question(self) -> None:
-        # Display the current question on the screen
-        text, key = st.session_state['questions'][st.session_state['interview_step']]
+        """
+        Display the current question.
+        """
+        text, key = self.session_state['questions'][self.session_state['interview_step']]
         message(text, key=key)
 
     def get_answer(self) -> None:
-        # Get the answer inputted by the user
-        answer = st.text_input("Your answer: ", key="input" + str(st.session_state['interview_step']))
+        """
+        Get and store the answer provided by the user.
+        """
+        answer = st.text_input("Your answer: ", key="input" + str(self.session_state['interview_step']))
         if answer:
-            # Store the answer and increment the interview step
-            st.session_state['answers'].append((answer, str(uuid.uuid4())))
-            st.session_state['interview_step'] += 1
+            self.session_state['answers'].append((answer, self._generate_uuid()))
+            self.session_state['interview_step'] += 1
             st.experimental_rerun()
 
     def display_past_questions_and_answers(self) -> None:
-        # Display all past questions and answers
-        for i in range(st.session_state['interview_step']):
-            question_text, question_key = st.session_state['questions'][i]
+        """
+        Display all past questions and their corresponding answers.
+        """
+        for i in range(self.session_state['interview_step']):
+            question_text, question_key = self.session_state['questions'][i]
             message(question_text, key=question_key)
-            if i < len(st.session_state['answers']):
-                answer_text, answer_key = st.session_state['answers'][i]
+            if i < len(self.session_state['answers']):
+                answer_text, answer_key = self.session_state['answers'][i]
                 message(answer_text, is_user=True, key=answer_key)
 
     def evaluate_candidate(self) -> str:
-        # Combine the questions and answers into a single string for evaluation
-        interview_text = ""
-        for (question, _), (answer, _) in zip(st.session_state['questions'], st.session_state['answers']):
-            interview_text += f"Question: {question}\nAnswer: {answer}\n"
-      
-        # Use the OpenAI API to generate an evaluation of the candidate
+        """
+        Generate an evaluation for the candidate based on their answers to the interview questions.
+        """
+        interview_text = "".join([f"Question: {question}\nAnswer: {answer}\n" for (question, _), (answer, _) in zip(self.session_state['questions'], self.session_state['answers'])])
+        
         response = get_completion(Parameters.evaluation_prompt.format(job_description=Parameters.job_description, interview_text=interview_text))
 
         return response
 
-# Creating the chatbot interface
+    @staticmethod
+    def _generate_uuid() -> str:
+        """
+        Generate a unique identifier.
+        """
+        return str(uuid.uuid4())
+
+
+def create_bot() -> None:
+    """
+    Create an InterviewBot and manage its operation.
+    """
+    bot = InterviewBot()
+
+    if len(bot.session_state['questions']) == 0:
+        message("Hello! I'm your interviewer bot powered by OpenAI. I will ask you a few questions, and your responses will be evaluated. Let's get started.", key="greeting")
+        bot.conduct_interview()
+
+    bot.display_past_questions_and_answers()
+
+    if bot.session_state['interview_step'] < len(bot.session_state['questions']):
+        bot.ask_question()
+        bot.get_answer()
+    elif bot.session_state['interview_step'] == len(bot.session_state['questions']):
+        evaluation = bot.evaluate_candidate()
+        st.write(f"OpenAI GPT-3.5's evaluation: {evaluation}")
+        bot.session_state['interview_step'] += 1
+
+
 st.title("InterviewBot - AI Interview Chatbot")
-
-bot = InterviewBot()
-
-# Only conduct the interview once, when the application first starts
-if len(st.session_state['questions']) == 0:
-    message("Hello! I'm your interviewer bot powered by OpenAI. I will ask you a few questions, and your responses will be evaluated. Let's get started.", key="greeting")
-    bot.conduct_interview()
-
-# Display past questions and answers
-bot.display_past_questions_and_answers()
-
-# Process the next question and answer
-if st.session_state['interview_step'] < len(st.session_state['questions']):
-    bot.ask_question()
-    bot.get_answer()
-elif st.session_state['interview_step'] == len(st.session_state['questions']):
-     # Evaluate the candidate once all questions have been asked
-    evaluation = bot.evaluate_candidate()
-    st.write(f"OpenAI GPT-3.5's evaluation: {evaluation}")
-    st.session_state['interview_step'] += 1
+create_bot()
--- utils.py
+++ utils.py
@@ -1,18 +1,26 @@
 import openai
 import re
 from typing import List
-# openai.api_key = ''
+from openai.error import InvalidRequestError
 
 def get_completion(complete_prompt: str, model="gpt-3.5-turbo") -> str:
-    # Send a message to the OpenAI API to get a response
-    messages = [{"role": "user", "content": complete_prompt}]
-    response = openai.ChatCompletion.create(model=model, messages=messages,
-                                            temperature = 0)
-    #time.sleep(5)
-    return response.choices[0].message["content"]
+    """
+    Send a message to the OpenAI API to get a response.
+    """
+    try:
+        messages = [{"role": "user", "content": complete_prompt}]
+        response = openai.ChatCompletion.create(model=model, messages=messages,
+                                                temperature = 0)
+        return response.choices[0].message["content"]
+    
+    except InvalidRequestError as e:
+        print(f"Encountered an error: {e}")
+        return "Error: Input too long for model."
 
 
 def get_questions(text_questions: str) -> List[str]:
-    # Extract all questions from the text using regular expressions
+    """
+    Extract all questions from the text using regular expressions.
+    """
     question_list = re.findall(r'\d[.:\s]*(.*)', text_questions)
     return question_list
